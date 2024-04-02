7b8568dda16de05784f07e2184562dc9d8bc3e77
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-04-02 16:00:29 +0300

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,21 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string()
+        .email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "net", "ua"] },
+        })
+        .required(),
+    phone: Joi.number().required(),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+    name: Joi.string().min(3).max(30),
+    email: Joi.string().email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net", "ua"] },
+    }),
+    phone: Joi.number(),
+});
