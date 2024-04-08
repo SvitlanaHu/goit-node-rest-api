615fd96b42b006da1a768909f572d2d8137d237f
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-04-08 22:52:34 +0300

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,18 +1,10 @@
 import * as contactsService from "../services/contactsServices.js";
-import {
-    createContactSchema,
-    updateContactSchema,
-} from "../schemas/contactsSchemas.js";
 import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res, next) => {
     try {
         const result = await contactsService.listContacts();
-        res.json({
-            status: "success",
-            code: 200,
-            data: result,
-        });
+        res.json(result);
     } catch (error) {
         next(error);
     }
@@ -54,9 +46,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
     try {
-        const { error } = createContactSchema.validate(req.body);
-        if (error) throw HttpError(400, error.message);
-
         const result = await contactsService.addContact(req.body);
 
         res.status(201).json({
@@ -72,16 +61,29 @@ export const createContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
     try {
         const { id } = req.params;
-        const { error } = updateContactSchema.validate(req.body);
-        if (error) {
-            throw HttpError(400, error.message);
-        }
-
         const result = await contactsService.updateContact(id, req.body);
         if (!result) {
             throw HttpError(404);
         }
 
+        return res.json({
+            status: "success",
+            code: 200,
+            data: result,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateStatusContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const result = await contactsService.updateStatus(id, req.body);
+        if (!result) {
+            throw HttpError(404);
+        }
         return res.json({
             status: "success",
             code: 200,
